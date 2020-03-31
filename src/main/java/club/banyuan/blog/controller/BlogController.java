afea37d6a024380650c3782ebe1e8ca2e6a45f32
master_ting <dzg2998@gmail.com>
2020-03-31 09:16:29 +0800

    firstcommit

--- src/main/java/club/banyuan/blog/controller/BlogController.java
+++ src/main/java/club/banyuan/blog/controller/BlogController.java
@@ -1,10 +1,8 @@
 package club.banyuan.blog.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
